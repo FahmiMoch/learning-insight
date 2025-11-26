6c8596b3a2502df49e7fedd11bdd3c438a30258e
Mochamad Fahmi <151912818+FahmiMoch@users.noreply.github.com>
2025-11-26 18:45:43 +0700

    Create api.js for Axios API configuration

---
+++ src/services/api.js
@@ -0,0 +1,35 @@
+// src/services/api.js
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000", // base URL backend
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token"); 
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+  
+    if (error.response?.status === 401) {
+      // misal redirect ke login
+      console.log("Unauthorized! Redirecting to login...");
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
